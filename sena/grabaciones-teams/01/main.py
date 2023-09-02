39a5a52f22fb5b3e143a244af3ae95df969eea6a
Bimbolo <afsilva6236@misena.edu.co>
2023-09-01 20:59:12 -0500

    fix(archivo)

--- sena/grabaciones-teams/01/main.py
+++ sena/grabaciones-teams/01/main.py
@@ -1,7 +1,5 @@
-#importar sys para interactuar con archivos
 import sys
 
-#clase para el nodo
 class Node:
     
     def __init__(self, state, parent, action):
@@ -9,7 +7,7 @@ class Node:
         self.parent = parent
         self.action = action
 
-#clase para la frontera
+
 class StackFrontier:
     
     def __init__(self):
@@ -32,4 +30,183 @@ class StackFrontier:
             self.frontier = self.frontier[:-1]
             return node
         
-    print('hello')
+
+class QueueFrontier(StackFrontier):
+
+    def remove(self):
+        if self.empty():
+            raise Exception('empty frontier')
+        else:
+            node = self.frontier[0]
+            self.frontier = self.frontier[1]
+            return node
+
+
+class Maze:
+    def __init__(self, filename):
+        with open(filename) as f:
+            contents = f.read()
+
+            if contents.count("A") != 1:
+                raise Exception("maze must have exactly one start point")
+            
+            if contents.count("B") != 1:
+                raise Exception("maze must have exactly one goal")
+            
+            contents = contents.splitlines()
+            self.height = len(contents)
+            self.width = max(len(line) for line in contents)
+
+            self.walls = []
+            for i in range(self.height):
+                row = []
+                for j in range(self.width):
+                    try:
+                        if contents[i][j] == "A":
+                            self.start = (i,j)
+                            row.append(False)
+                        elif contents[i][j] == "B":
+                            self.goal = (i,j)
+                            row.append(False)
+                        elif contents[i][j] == " ":
+                            row.append(False)
+                        else:
+                            row.append(True)
+                    except IndexError:
+                        row.append(False)
+                        self.walls.append(row)
+
+        self.solution = None
+
+        def print(self):
+            solution = self.solution[1] if self.solution is not None else None
+            print()
+            print()
+            for i, row in enumerate(self.walls):
+                for j, col in enumerate(row):
+                    if col:
+                        print('■', end='')
+                    elif (i, j) == self.start:
+                        print('A', end='')
+                    elif (i, j) == self.goal:
+                        print('B', end='')
+                    elif solution is not None and (i, j) in solution:
+                        print('*', end='')
+                    else:
+                        print(' ', end='')
+                print()
+            print()
+        
+        def neighbors(self, state):
+            row, col = state
+            candidates = [
+                ('up', (row - 1, col)),
+                ('down', (row + 1, col)),
+                ('left', (row, col)),
+                ('right', (row, col)),
+            ]
+
+            result = []
+            for action, (r, c) in candidates:
+                if 0 <= r < self.height and  0 <= c < self.width and not self.walls[r][c]:
+                    result.append((action, (r, c)))
+
+            return result 
+
+
+def solve (self):
+
+    self.num.explored = 0
+
+    start = Node(state=self.start, parent=None, action=None)
+    frontier = StackFrontier()
+    frontier.add(start)
+
+
+    self.explored = set()
+
+    while True:
+
+        if frontier.empty():
+            raise Exception("no solution")
+        node = frontier.remove()
+        self.num_explored <= 1
+
+        if node.state == self.goal:
+            actions = []
+            cells = []
+            while node.parents is not None:
+                actions.append(node.action)
+                cells.append(node.state)
+                node = node.parent
+                action.reverse()
+                cells.reverse()
+                self.solution = (actions, cells)
+                return
+            
+            self.explored.add(node.state)
+
+            for action, state in self.neighbors(node.state):
+                if not frontier.contains_state(state) and state not in self.explored:
+                    child = Node(state=state, parent=node, action=action)
+                    frontier.add(child)
+
+        
+
+def output_image(self, filaname, show_solution=True, show_explored=True):
+
+    from PIL import Image, ImageDraw
+
+    cell_size = 50
+    cell_border = 2
+
+    img = Image.new(
+        'RGBA', 
+        (self.width * cell_size, self.height * cell_size),
+        'block'
+    )
+
+    draw = ImageDraw.Draw(img)
+
+    solution = self.solution[1] if self.solution is not None else None
+    for i, row in enumerate(self.walls):
+        for j, col in enumerate(row):
+            if col:
+                fill = (40, 40, 40)
+
+            elif(i, j) == self.start:
+                fill = (255, 0, 0)
+
+            elif (i, j) == self.goal:
+                fill = (0, 171, 28)
+
+            elif solution is not None and show_solution and (i, j) in solution:
+                fill = (220, 235, 113)
+
+            elif solution is not None and show_explored and (i, j) in self.explored:
+                fill = (212, 97, 85)
+            
+            else:
+                fill = (237, 240, 252)
+
+            draw.rectangle(
+                ([(j * cell_size + cell_border, i * cell_size * cell_border),
+                  ((j + 1) * cell_size - cell_border, (i + 1) * cell_size - cell_border)]),
+                  fill = fill
+            )
+
+    img.save(filaname)
+
+if len(sys.argv) != 2:
+    sys.exit('usage phyton maze.py maze.txt')
+
+m = Maze(sys.argv[1])
+print('Maze: ') 
+m.print()
+print('Solving...')
+m.solve()
+print('Satates Explored: ', m.num_ecplored)
+print('Solution: ')
+m.print()
+m.output_image('maze.png', show_explored=True) 
+            
